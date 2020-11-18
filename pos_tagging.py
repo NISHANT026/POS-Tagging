a41b8db6516ec58caf105fbdf68ee8c48768ef54
D-setia <dsetia@cs.iitr.ac.in>
2020-11-18 18:05:27 +0530

    Organise code

--- pos_tagging.py
+++ pos_tagging.py
@@ -11,7 +11,7 @@ def parseXML():
  files = ["A1", "A2", "A3","A4","A5","A6","A7", "A8", "A9","AA","AB","AC","AD", "AH", "AJ","AK","AL","AM"]
  
  for x in files:
-   path = '../Train-corups/'+x
+   path = 'Train-corpus/'+x
 
    for filename in os.listdir(path):
     if not filename.endswith('.xml'): continue
@@ -24,39 +24,35 @@ def parseXML():
     words = []
     for word in root.iter('w'):
         wo=word.get('hw')
-        wo = word.text
-        if(wo[len(wo)-1] == ' ') or (wo[len(wo)-1] == '\n'):
-            wo = wo[:len(wo)-1]
-        wo = wo.lower()
-        #print(word.text)
-        #print(len(word.text))
         ta=word.get('c5')
-        ta1 = ta
-        ta2 = None
-        if len(ta)>3:
-            ta1 = ta[:3]
-            ta2 = ta[4:]
-        else:
-            ta2 = None
         encoding=wo+"_"+ta
        # print(wo)
-        if not wo in worddict:
-         worddict[wo]=1
-        else:
-         worddict[wo]+=1
-        if not ta1 in tagdict:
-         tagdict[ta1]=1
-        else:
-         tagdict[ta1]+=1
-        if ta2 != None:
-            if not ta2 in tagdict:
-                tagdict[ta2]=1
+        if (ta.find('-') !=-1):
+            t1=ta[0:3]
+            t2=ta[-3:]
+            #print(encoding,t1,t2)
+            if not t1 in tagdict:
+             tagdict[t1]=1
             else:
-                tagdict[ta2]+=1
+             tagdict[t1]+=1
+            if not t2 in tagdict:
+             tagdict[t2]=1
+            else:
+             tagdict[t2]+=1
+        else:
+            if not wo in worddict:
+             worddict[wo]=1
+            else:
+             worddict[wo]+=1
+            if not ta in tagdict:
+             tagdict[ta]=1
+            else:
+             tagdict[ta]+=1
         # encoding = word.get('hw') + " " + word.get('c5')
         # Word[wo]+=1
         #Tag[ta]+=1
         words.append(encoding)
+      #  print(ta)
         #print(encoding)
 parseXML()
 #print(worddict)
@@ -73,6 +69,6 @@ for x in range(10):
  print(Key,tagdict[Key])
  del tagdict[Key]
 
-
+#print(worddict["that"])
  
 
