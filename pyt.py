12e635760a0343d66a628ed20439d987c374f1a4
D-setia <dsetia@cs.iitr.ac.in>
2020-11-25 22:24:15 +0530

    Add Submission 2 code

--- pyt.py
+++ pyt.py
@@ -1,7 +1,8 @@
 import csv
 import xml.etree.ElementTree as ET
 import os
-
+import numpy as np
+from collections import defaultdict
 worddict = {}
 tagdict = {}
 tagDictPerWord = {}
@@ -15,9 +16,9 @@ def parseXML():
         for filename in os.listdir(path):
             if not filename.endswith('.xml'): continue
             fullname = os.path.join(path, filename)
-        
+   
             tree = ET.parse(fullname)
-    
+
             root = tree.getroot()
             words = []
             for word in root.iter('w'):
@@ -28,7 +29,7 @@ def parseXML():
                     wo = wo[:len(wo)-1]
                 ta=word.get('c5')
                 encoding=wo+"_"+ta
-        
+
                 if (ta.find('-') !=-1):
                     t1=ta[0:3]
                     t2=ta[-3:]
@@ -41,9 +42,9 @@ def parseXML():
                         tagdict[t2]=1
                     else:
                         tagdict[t2]+=1
-        
+
                     if not wo in tagDictPerWord:
-                         tagDictPerWord[wo] = {t1: 1, t2: 1}
+                        tagDictPerWord[wo] = {t1: 1, t2: 1}
                     else:
                         if not t1 in tagDictPerWord[wo]:
                             tagDictPerWord[wo][t1] = 1
@@ -53,7 +54,7 @@ def parseXML():
                             tagDictPerWord[wo][t2] = 1
                         else:
                             tagDictPerWord[wo][t2] += 1
-    
+
                 else:
                     if not wo in worddict:
                         worddict[wo]=1
@@ -63,7 +64,7 @@ def parseXML():
                         tagdict[ta]=1
                     else:
                         tagdict[ta]+=1
-    
+
                     if not wo in tagDictPerWord:
                         tagDictPerWord[wo] = {ta: 1}
                     else:
@@ -73,7 +74,7 @@ def parseXML():
                             tagDictPerWord[wo][ta] += 1
         
         words.append(encoding)
-
+        
 def printTop10():
     print("Word")
     for x in range(10):
@@ -104,16 +105,20 @@ def runOnTestData():
     correct = 0
     total = 0
     files = ["AN", "AP", "AR","AS","AT","AY"]
- 
+
+    confusionmatrix=defaultdict(lambda:defaultdict(int))
     for x in files:
         path = 'Test-corpus/'+x
 
+        #for filename in os.listdir(path):
+         #if not filename.endswith('.xml'): continue
+         #fullname = os.path.join(path, filename)
         for filename in os.listdir(path):
             if not filename.endswith('.xml'): continue
             fullname = os.path.join(path, filename)
-   
-            tree = ET.parse(fullname)
     
+            tree = ET.parse(fullname)
+
             root = tree.getroot()
             words = []
             mostPropTag = ''
@@ -129,35 +134,70 @@ def runOnTestData():
                 if (ta.find('-') !=-1):
                     t1=ta[0:3]
                     t2=ta[-3:]
-
+                    actual1=t1
+                    actual2=t2 
                     if not wo in tagDictPerWord:
-                        if ta.find("VVB") != -1:
+                        if ta.find("NN1") != -1:
                             correct += 1
+                        predicted="NN1"
+                        confusionmatrix[actual1][predicted]+=1
+                        confusionmatrix[actual2][predicted]+=1
+
+                           # print(correct)
                     else:
                         mostProbTag = findMostProbTag(tagDictPerWord[wo])
-                        if t1 == mostProbTag or t2 == mostProbTag:
+                        if t1 == mostProbTag: 
                             correct += 1
+                        predicted=mostProbTag
+                        confusionmatrix[actual1][predicted]+=1
+                        if t2 == mostProbTag:
+                            correct += 1
+                        predicted=mostProbTag
+                        confusionmatrix[actual2][predicted]+=1  
 
                 else:
+                    actual=ta
                     if not wo in tagDictPerWord:
-                        if ta == "VVB":
+                        if ta == "NN1":
                             correct += 1
+                        predicted="NN1"
+                        confusionmatrix[actual][predicted]+=1
                     else:
                         mostProbTag = findMostProbTag(tagDictPerWord[wo])
                         if ta == mostProbTag:
                             correct += 1
-    print(correct/total*100)
+                        predicted=mostProbTag
+                        confusionmatrix[actual][predicted]+=1	
+    total_=0
+    correct_=0
+    accuracy_=0
+
+    for i in  confusionmatrix:
+        for j in confusionmatrix[i]:
+            total_ += confusionmatrix[i][j]
+        correct_ += confusionmatrix[i][i]
+	#print("\n")
+    prettymaxtrix=[]
+    for tagi,freqi in tagdict.items(): 
+        temp=[]
+        for tagj,freqj in tagdict.items():
+            temp.append(confusionmatrix[tagi][tagj])
+        prettymaxtrix.append(temp)
+    #print(prettymaxtrix)
+ 	
+    accuracy_=correct_*100
+    acc=correct*100
+    print(acc/total)
     print(correct)
     print(total)
+    #print(correct/total)
+    print(prettymaxtrix)
 
 
 def main():
     parseXML()
-    #printTop10()
+   # printTop10()
     runOnTestData()
-    print(tagdict.keys())
 
 
 main()
- 
-
